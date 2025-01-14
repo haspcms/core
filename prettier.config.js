e732dc829800aa51851cb7d2f6b4bb132a0263c9
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-14 09:59:01 +0800

    chore: update prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tailwindAttributes: [
+    "class",
+    "className",
+    "ngClass",
+    ".*Class.*",
+    ".*Classes.*",
+  ],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
