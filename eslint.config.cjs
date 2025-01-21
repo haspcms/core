376a4f4358ecf520e7d879a041f99f2f23d966e0
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-21 13:05:55 +0800

    chore: update project

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+export default [
+  {
+    rules: {
+      "no-unused-vars": ["warn", { args: "none" }],
+      eqeqeq: ["warn", "always"],
+      "no-unexpected-multiline": "warn",
+      "no-cond-assign": ["warn", "always"],
+      "no-redeclare": "warn",
+      "no-self-compare": "warn",
+    },
+  },
+];
