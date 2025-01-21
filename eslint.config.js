e260c6e8e2fa9d4aadb2131226a9a2ebd7d376ec
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-21 12:43:43 +0800

    chore: update formatters

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-module.exports = [
-  {
-    rules: {
-      semi: "error",
-      "prefer-const": "error",
-    },
+module.exports = {
+  rules: {
+    "no-unused-vars": ["warn", { args: "none" }],
+    eqeqeq: ["warn", "always"],
+    "no-unexpected-multiline": "warn",
+    "no-cond-assign": ["warn", "always"],
+    "no-redeclare": "warn",
+    "no-self-compare": "warn",
   },
-];
+};
