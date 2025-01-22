627d387081079b717509553bdcf376d72bf5ef15
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-22 14:53:03 +0800

    chore: update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import rc from "rc";
-
-var confJSON = rc("hasp");
-const config = JSON.stringify(confJSON, null, 2);
-console.log({ config });
+// var confJSON = rc("hasp");
+// const config = JSON.stringify(confJSON, null, 2);
+// console.log({ config });
 
 console.log("contents:", config?.contents);
 console.log("[INFO] HASP Core started.");
