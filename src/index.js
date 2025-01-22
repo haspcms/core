c81c952c1c610873a0aebae46cb8ccf414cfed95
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-22 14:47:31 +0800

    chore: update

--- src/index.js
+++ src/index.js
@@ -1,47 +1,10 @@
-// import findupSync from "findup-sync";
+import rc from "rc";
 
-// import rc from "rc";
+var confJSON = rc("hasp");
+const config = JSON.stringify(confJSON, null, 2);
+console.log({ config });
 
-// // Load configuration
-// var configuration = rc("hasp");
-
-// console.log(JSON.stringify(configuration, null, 2)); // Display configuration as a JSON string
-// console.log({ configuration }); // Log configuration as an object
-
-import { cosmiconfig } from "cosmiconfig";
-// ...
-const explorer = cosmiconfig("hasp");
-console.log({ explorer });
-
+console.log("contents:", config?.contents);
 console.log("[INFO] HASP Core started.");
-
-// const filePath = findupSync("filename");
-// console.log({ filePath });
-
-// import { promises as fs } from "fs";
-
-// export async function loadConfig() {
-//   const configFileName = "hasp.config.js";
-//   const configPath = path.resolve(process.cwd(), configFileName);
-
-//   try {
-//     await fs.access(configPath);
-
-//     const configModule = await import(configPath);
-//     const config = configModule.default || configModule;
-
-//     if (typeof config !== "object" || config === null) {
-//       throw new Error("Configuration file does not export an object.");
-//     }
-
-//     return config;
-//   } catch (error) {
-//     console.error(
-//       "Error loading configuration:",
-//       error instanceof Error ? error.message : error,
-//     );
-//     return {};
-//   }
-// }
 
 export * from "./lib";
