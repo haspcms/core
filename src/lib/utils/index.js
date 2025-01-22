59f8e2c1e6ca329b192cd0ed8c36fd4cba399e22
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-22 15:46:42 +0800

    chore: update

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -4,7 +4,14 @@ const APP_NAME = "hasp";
 const confJSON = rc(APP_NAME);
 
 export const loadConfig = () => {
-  return confJSON;
+  const configString = JSON.stringify(confJSON, null, 2);
+  console.log({ configString });
+
+  // Access the config as a JavaScript object
+  const configObject = JSON.parse(configString);
+  console.log({ configObject });
+
+  return configObject;
 };
 
 export function sortBlocks(list) {
