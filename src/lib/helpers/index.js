eed4707a5da4219afbd24d318418fbd66b1be428
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-22 11:29:23 +0800

    chore: update

--- src/lib/helpers/index.js
+++ src/lib/helpers/index.js
@@ -1,30 +1,29 @@
-import { promises as fs } from "fs";
-import path from "path";
+// import { promises as fs } from "fs";
 
-export async function loadConfig() {
-  const configFileName = "hasp.config.js";
-  const configPath = path.resolve(process.cwd(), configFileName);
+// export async function loadConfig() {
+//   const configFileName = "hasp.config.js";
+//   const configPath = path.resolve(process.cwd(), configFileName);
 
-  try {
-    await fs.access(configPath);
+//   try {
+//     await fs.access(configPath);
 
-    // Using dynamic import for compatibility with ESM
-    const configModule = await import(configPath);
-    const config = configModule.default || configModule;
+//     // Using dynamic import for compatibility with ESM
+//     const configModule = await import(configPath);
+//     const config = configModule.default || configModule;
 
-    if (typeof config !== "object" || config === null) {
-      throw new Error("Configuration file does not export an object.");
-    }
+//     if (typeof config !== "object" || config === null) {
+//       throw new Error("Configuration file does not export an object.");
+//     }
 
-    return config;
-  } catch (error) {
-    console.error(
-      "Error loading configuration:",
-      error instanceof Error ? error.message : error,
-    );
-    return {};
-  }
-}
+//     return config;
+//   } catch (error) {
+//     console.error(
+//       "Error loading configuration:",
+//       error instanceof Error ? error.message : error,
+//     );
+//     return {};
+//   }
+// }
 
 export const consoleUtils = () => {
   console.log("consoleUtils");
