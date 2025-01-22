3529df00898f567f31da2fcb40375764e8dcdb34
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-01-22 15:50:00 +0800

    chore: update

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@ export * from "./api";
 export * from "./props";
 export * from "./services";
 export * from "./utils";
+export * from "./utils/loadConfig";
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,19 +1,3 @@
-import rc from "rc";
-
-const APP_NAME = "hasp";
-const confJSON = rc(APP_NAME);
-
-export const loadConfig = () => {
-  const configString = JSON.stringify(confJSON, null, 2);
-  console.log({ configString });
-
-  // Access the config as a JavaScript object
-  const configObject = JSON.parse(configString);
-  console.log({ configObject });
-
-  return configObject;
-};
-
 export function sortBlocks(list) {
   return list.sort((a, b) => a.order - b.order);
 }
