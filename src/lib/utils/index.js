a898e6894a50ad974cfbe0d66df21e62b9fc740c
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-02-14 11:11:40 +0800

    chore: update

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -2,15 +2,15 @@ export function sortBlocks(list) {
   return list.sort((a, b) => a.order - b.order);
 }
 
-import { cosmiconfig } from "cosmiconfig";
+// import { cosmiconfig } from "cosmiconfig";
 
-export async function loadHaspConfig() {
-  const explorer = cosmiconfig("hasp");
-  const result = await explorer.search();
+// export async function loadHaspConfig() {
+//   const explorer = cosmiconfig("hasp");
+//   const result = await explorer.search();
 
-  if (!result || result.isEmpty) {
-    throw new Error("hasp.config.js not found or empty.");
-  }
+//   if (!result || result.isEmpty) {
+//     throw new Error("hasp.config.js not found or empty.");
+//   }
 
-  return result.config;
-}
+//   return result.config;
+// }
