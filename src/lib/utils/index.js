fef60ab14292bc81f265ffd80da79808f801abb8
Raul Jr. Comeros <comeros.halcyondigital@gmail.com>
2025-02-14 10:26:53 +0800

    chore: update

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -2,11 +2,11 @@ export function sortBlocks(list) {
   return list.sort((a, b) => a.order - b.order);
 }
 
-import { cosmiconfigSync } from "cosmiconfig";
+import { cosmiconfig } from "cosmiconfig";
 
-export function loadHaspConfig() {
-  const explorer = cosmiconfigSync("hasp"); // Looks for hasp.config.js
-  const result = explorer.search();
+export async function loadHaspConfig() {
+  const explorer = cosmiconfig("hasp");
+  const result = await explorer.search();
 
   if (!result || result.isEmpty) {
     throw new Error("hasp.config.js not found or empty.");
